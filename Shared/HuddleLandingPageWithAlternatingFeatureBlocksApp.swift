4b1470b88cea92c81e2ad02ac1b8ebccd3623fcc
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-16 10:23:03 +0900

    Initial Commit

---
+++ Shared/HuddleLandingPageWithAlternatingFeatureBlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuddleLandingPageWithAlternatingFeatureBlocksApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct HuddleLandingPageWithAlternatingFeatureBlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
